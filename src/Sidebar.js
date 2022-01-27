bb99abe55c0587e53bc44534c0bf795ed79c3542
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-01-27 23:03:01 +0500

    Complete Side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,13 +4,21 @@ import Sidebarrow from './Sidebarrow';
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import { ExpandMoreOutlined, History, OndemandVideo, ThumbUpOutlined, VideoLibrary, WatchLater } from '@material-ui/icons';
 const Sidebar = () => {
   return (
   <div className='Sidebar'>
      
-    <Sidebarrow Icons={HomeIcon} title="Home"/>
+    <Sidebarrow Selected Icons={HomeIcon} title="Home"/>
     <Sidebarrow Icons={WhatshotIcon} title="Trending"/>
     <Sidebarrow Icons={SubscriptionsIcon} title="Subscritption"/>
+    <hr/>
+    <Sidebarrow Icons={VideoLibrary} title="Library"/>
+    <Sidebarrow Icons={History} title="History"/>
+    <Sidebarrow Icons={OndemandVideo} title="Your videos"/>
+    <Sidebarrow Icons={WatchLater} title="Watch later"/>
+    <Sidebarrow Icons={ThumbUpOutlined} title="Liked videos"/>
+    <Sidebarrow Icons={ExpandMoreOutlined} title="Show more"/>
   </div>);
 };
 
--- src/Sidebarrow.js
+++ src/Sidebarrow.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './Sidebarrow.css'
-const Sidebarrow = ({Icons,title}) => {
+const Sidebarrow = ({Selected,Icons,title}) => {
   return (
-  <div className='Sidebarrow'>
-      <Icons/>
-      <h2>{title}</h2>
+  <div className={`Sidebarrow ${Selected && "Selected"}`}>
+      <Icons className='Sidebarrow_icons'/>
+      <h2 className='Sidebarrow_title'>{title}</h2>
   </div>);
 };
 
