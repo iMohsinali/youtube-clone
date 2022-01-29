b117bc1912aa91bd41012d637c46d1ca982d11f6
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-01-29 23:46:28 +0500

    React Router and Usestate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Recommand from "./Recommand";
+import Sidebar from "./Sidebar";
 
-import './App.css';
-import Header from './Header';
-import Recommand from './Recommand';
-import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="App">
-        <Header/>  
-         <div className='app_page'>
-           <Sidebar/>
-           <Recommand/>
-         </div>
+      <Router>
+      <Header />
+        <Routes>
+          <Route path="/search/:searchTerm" element={"hello"} >
+          
+          </Route>
+          <Route path='/' element ={<div className="app_page">
+        <Sidebar />
+        <Recommand />
+        
+      </div>}>
 
+          </Route>
+        </Routes>
+      </Router>
+
+     
+      
+     
     </div>
- 
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,10 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
+  const [inputsearch,setsearch]=useState('')
   return (
     <div className="header">
       <div className="header_left">
@@ -17,8 +20,11 @@ const Header = () => {
       </div>
 
       <div className="header_input">
-        <input placeholder="Search" type="text" />
+        <input onChange={e=>setsearch(e.target.value)} value={inputsearch} placeholder="Search" type="text" />
+        <Link to={`/search/${inputsearch}`}>
         <SearchIcon className="headerinputbtn" />
+        </Link>
+       
       </div>
       <div className="header_icons">
         <VideoCallIcon className="header_icon" />
